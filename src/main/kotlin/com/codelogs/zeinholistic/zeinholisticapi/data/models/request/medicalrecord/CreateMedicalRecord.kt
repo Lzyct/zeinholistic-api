2839b986ad92f3ab968027abdc0d62ea7ffeccee
ukieTux <hey.mudassir@gmail.com>
2021-04-25 17:14:24 +0800

    Create Medical Record Request Model

---
+++ src/main/kotlin/com/codelogs/zeinholistic/zeinholisticapi/data/models/request/medicalrecord/CreateMedicalRecord.kt
@@ -0,0 +1,27 @@
+package com.codelogs.zeinholistic.zeinholisticapi.data.models.request.medicalrecord
+
+/**
+ **********************************************
+ *  zeinholistic-api
+ *  |-> com.codelogs.zeinholistic.zeinholisticapi.data.models.request.medicalrecord
+ *    |-> CreateMedicalRecord.kt
+ * --------------------------------------------
+ * Created by Mudassir 🧑🏻‍💻
+ * on 📅 25/04/21 🕰 17:00 with ❤️
+ * email : hey.mudassir@gmail.com
+ * github : https://www.github.com/ukieTux
+ **********************************************
+ * © 2021 | All Right Reserved
+ */
+
+data class CreateMedicalRecord(
+    val idPatient: String,
+    val mainComplaint: String,
+    val additionalComplaint: String? = "",
+    val historyOfDisease: String? = "",
+    val checkUpResult: String? = "",
+    val conclusionDiagnosis: String? = "",
+    val suggestion: String? = "",
+    val examiner: String? = "",
+    val createdAt: String,
+)
---
+++ src/main/kotlin/com/codelogs/zeinholistic/zeinholisticapi/data/models/request/medicalrecord/DeleteMedicalRecord.kt
@@ -0,0 +1,19 @@
+package com.codelogs.zeinholistic.zeinholisticapi.data.models.request.medicalrecord
+
+/**
+ **********************************************
+ *  zeinholistic-api
+ *  |-> com.codelogs.zeinholistic.zeinholisticapi.data.models.request.medicalrecord
+ *    |-> CreateMedicalRecord.kt
+ * --------------------------------------------
+ * Created by Mudassir 🧑🏻‍💻
+ * on 📅 25/04/21 🕰 17:00 with ❤️
+ * email : hey.mudassir@gmail.com
+ * github : https://www.github.com/ukieTux
+ **********************************************
+ * © 2021 | All Right Reserved
+ */
+
+data class DeleteMedicalRecord(
+    val idPatient: String,
+)
