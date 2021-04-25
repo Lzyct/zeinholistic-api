f8abb609493dc78405c3b192c04842bb67723dff
ukieTux <hey.mudassir@gmail.com>
2021-04-25 17:14:34 +0800

    Create Patient Request Model

---
+++ src/main/kotlin/com/codelogs/zeinholistic/zeinholisticapi/data/models/request/patient/DeletePatientRequest.kt
@@ -0,0 +1,20 @@
+package com.codelogs.zeinholistic.zeinholisticapi.data.models.request.patient
+
+
+/**
+ **********************************************
+ *  zeinholistic-api
+ *  |-> com.codelogs.zeinholistic.zeinholisticapi.data.models.request.patient
+ *    |-> CreatePatientRequest.kt
+ * --------------------------------------------
+ * Created by Mudassir 🧑🏻‍💻
+ * on 📅 25/04/21 🕰 16:53 with ❤️
+ * email : hey.mudassir@gmail.com
+ * github : https://www.github.com/ukieTux
+ **********************************************
+ * © 2021 | All Right Reserved
+ */
+
+data class DeletePatientRequest(
+    val id: String,
+)
