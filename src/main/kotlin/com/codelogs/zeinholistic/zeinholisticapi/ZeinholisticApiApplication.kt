96a1d16316cd349a418ba4d493eedb2cca74ac32
ukieTux <hey.mudassir@gmail.com>
2021-04-25 16:32:42 +0800

    Setup project

---
+++ src/main/kotlin/com/codelogs/zeinholistic/zeinholisticapi/ZeinholisticApiApplication.kt
@@ -0,0 +1,11 @@
+package com.codelogs.zeinholistic.zeinholisticapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZeinholisticApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZeinholisticApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/codelogs/zeinholistic/zeinholisticapi/ZeinholisticApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codelogs.zeinholistic.zeinholisticapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZeinholisticApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
