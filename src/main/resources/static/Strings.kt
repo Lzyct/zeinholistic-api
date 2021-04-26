a3dfd63a21ab8d13633ee9b8c369cca278e7b040
ukieTux <hey.mudassir@gmail.com>
2021-04-26 13:27:35 +0800

    Create const variable

---
+++ src/main/resources/static/Strings.kt
@@ -0,0 +1,21 @@
+package static
+
+/**
+ **********************************************
+ *  zeinholistic-api
+ *  |-> static
+ *    |-> Strings.kt
+ * --------------------------------------------
+ * Created by Mudassir 🧑🏻‍💻
+ * on 📅 26/04/21 🕰 11:32 with ❤️
+ * email : hey.mudassir@gmail.com
+ * github : https://www.github.com/ukieTux
+ **********************************************
+ * © 2021 | All Right Reserved
+ */
+object Strings {
+    const val APP_JSON = "application/json"
+    const val SUCCESS = "Success"
+    const val ERROR = "Error"
+    const val NOT_FOUND = "Data not found"
+}
