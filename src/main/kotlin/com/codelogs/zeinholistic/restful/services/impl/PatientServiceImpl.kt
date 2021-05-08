2250979b4ee357a4ebca338e3ad7a545643d530f
ukieTux <hey.mudassir@gmail.com>
2021-05-08 13:15:29 +0800

    Add comment for transactional annotation

--- src/main/kotlin/com/codelogs/zeinholistic/restful/services/impl/PatientServiceImpl.kt
+++ src/main/kotlin/com/codelogs/zeinholistic/restful/services/impl/PatientServiceImpl.kt
@@ -34,7 +34,7 @@ import java.util.stream.Collectors
  */
 
 @Service
-@Transactional
+@Transactional //Add transactional annotation to enable multiple query
 class PatientServiceImpl(
     val patientRepository: PatientRepository,
     val medicalRecordRepository: MedicalRecordRepository,
