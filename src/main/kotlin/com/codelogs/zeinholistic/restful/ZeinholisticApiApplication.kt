d8b1ccb6f1edd1b01ecc87fe131ee03be009d179
ukieTux <hey.mudassir@gmail.com>
2021-05-08 22:58:09 +0800

    Fix CORS issue on Web

--- src/main/kotlin/com/codelogs/zeinholistic/restful/ZeinholisticApiApplication.kt
+++ src/main/kotlin/com/codelogs/zeinholistic/restful/ZeinholisticApiApplication.kt
@@ -2,10 +2,29 @@ package com.codelogs.zeinholistic.restful
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
+
 
 @SpringBootApplication
-class ZeinholisticApiApplication
+class ZeinholisticApiApplication{
+
+    // Override allow origin to url client to fix CORS
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:62180")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<ZeinholisticApiApplication>(*args)
+    runApplication<ZeinholisticApiApplication>(*args)
 }
