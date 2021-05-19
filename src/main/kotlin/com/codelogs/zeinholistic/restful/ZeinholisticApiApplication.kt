0a46daabcdb69b5b820ff7ad0be02eb0339848c2
ukieTux <hey.mudassir@gmail.com>
2021-05-19 23:57:50 +0800

    Change allow orogin to *, set TZ to GMT+8

--- src/main/kotlin/com/codelogs/zeinholistic/restful/ZeinholisticApiApplication.kt
+++ src/main/kotlin/com/codelogs/zeinholistic/restful/ZeinholisticApiApplication.kt
@@ -3,12 +3,15 @@ package com.codelogs.zeinholistic.restful
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.*
+import javax.annotation.PostConstruct
 
 
 @SpringBootApplication
+@ComponentScan
 class ZeinholisticApiApplication {
 
     // Override allow origin to url client to fix CORS
@@ -16,11 +19,17 @@ class ZeinholisticApiApplication {
     fun corsConfigurer(): WebMvcConfigurer? {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8181")
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
-
             }
         }
+    }
+
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"))
+        println("Spring boot application running in UTC timezone :" + Date())
     }
 }
 
